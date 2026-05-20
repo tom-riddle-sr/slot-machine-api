f007366c7cc3eb17b555d058110c46ff01e18a2a
loti <lotichang@yile.com.tw>
2026-05-20 10:19:09 +0800

    init slot machine project

---
+++ src/main/java/com/example/slot_machine/SlotMachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.slot_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slot_machine/SlotMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slot_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
